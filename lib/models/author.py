70e42ab15514505ced85e799c078dd883805edb1
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-05-26 12:24:03 +0300

    The articles

---
+++ lib/models/author.py
@@ -0,0 +1,82 @@
+from lib.db.connection import get_connection
+from lib.models.article import Article
+from lib.models.magazine import Magazine
+
+class Author:
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name.strip() if name else None
+        if self.name == '':
+            raise ValueError("Author name cannot be empty")
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        if self.id is None:
+            cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute("UPDATE authors SET name=? WHERE id=?", (self.name, self.id))
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE id=?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row['id'], name=row['name'])
+        return None
+
+    @classmethod
+    def find_by_name(cls, name):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE name=?", (name,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row['id'], name=row['name'])
+        return None
+
+    def articles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id=?", (self.id,))
+        articles = [Article(id=row['id'], title=row['title'], author_id=row['author_id'], magazine_id=row['magazine_id']) for row in cursor.fetchall()]
+        conn.close()
+        return articles
+
+    def magazines(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.* FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        magazines = [Magazine(id=row['id'], name=row['name'], category=row['category']) for row in cursor.fetchall()]
+        conn.close()
+        return magazines
+
+    def add_article(self, magazine, title):
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Expected a Magazine instance")
+        article = Article(title=title, author_id=self.id, magazine_id=magazine.id)
+        article.save()
+        return article
+
+    def topic_areas(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.category FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        categories = [row['category'] for row in cursor.fetchall()]
+        conn.close()
+        return categories
